60a0da38d910c7d009393da48de8562dea14b9f4
yueyinqiu <yueyinqiu@outlook.com>
2026-08-05 15:08:32 +0800

    f

--- src/packages/mindustry/default.nix
+++ src/packages/mindustry/default.nix
@@ -1,6 +1,6 @@
 { nixpkgs-mindustry, ... }: {
   home.packages = [
-    nixpkgs-mindustry.mindustry-wayland
+    # nixpkgs-mindustry.mindustry-wayland
   ];
   my.navi-cheats.mindustry = ''
     # run mindustry, a sandbox tower-defense game.
