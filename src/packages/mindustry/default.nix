6aa03d943753afa411a57661ce1b53587188b9b7
yueyinqiu <yueyinqiu@outlook.com>
2026-08-05 14:37:21 +0800

    remove mindustry

--- src/packages/mindustry/default.nix
+++ src/packages/mindustry/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    mindustry-wayland
+    # mindustry-wayland
   ];
   my.navi-cheats.mindustry = ''
     # run mindustry, a sandbox tower-defense game.
