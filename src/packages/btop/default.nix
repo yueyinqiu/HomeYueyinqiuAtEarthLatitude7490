4e15849558184faea82f57a44a8ab61f3ab39d80
yueyinqiu <yueyinqiu@outlook.com>
2026-08-07 00:18:17 +0800

    btop

--- src/packages/btop/default.nix
+++ src/packages/btop/default.nix
@@ -2,6 +2,10 @@
   home.packages = with pkgs; [
     btop
   ];
+  imports = [
+    ./cheats
+  ];
+
   my.navi-cheats.btop = ''
     # run btop, a resource monitor that shows usage and stats for processor, memory, disks, network and processes.
     btop
