24dcbdf22017988bfe4e4a0339621d00f7bd65ad
yueyinqiu <yueyinqiu@outlook.com>
2026-08-06 23:58:08 +0800

    up or down volumne?

--- src/packages/avizo/cheats/default.nix
+++ src/packages/avizo/cheats/default.nix
@@ -1,22 +1,4 @@
 { ... }: {
-  services.avizo.enable = true;
-
-  my.navi-cheats.avizo = ''
-    $ direction: printf "up\ndown\n" --- --map my-bash-escape-std
-
-    # adjust speaker volume with avizo notification
-    volumectl -u <direction>
-
-    # toggle speaker mute state with avizo notification
-    volumectl toggle-mute
-
-    # toggle microphone mute state with avizo notification
-    volumectl -m toggle-mute
-
-    # adjust screen brightness with avizo notification
-    lightctl <direction>
-  '';
-
   my.snavi-cheats = [
     {
       cheat = builtins.toJSON {
@@ -42,9 +24,7 @@
         ExtraArguments = true;
       };
       extraFiles = {
-        "up-or-down.cs" = ''
-          
-        '';
+        "up-or-down.cs" = builtins.readFile ./up-or-down.cs;
       };
     }
   ];
