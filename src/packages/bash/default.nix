52f1898270414284077a4ac3c94e2500f27706d5
yueyinqiu <yueyinqiu@outlook.com>
2026-08-07 00:17:08 +0800

    bash

--- src/packages/bash/default.nix
+++ src/packages/bash/default.nix
@@ -11,6 +11,10 @@
     })
   ];
 
+  imports = [
+    ./cheats
+  ];
+
   my.navi-cheats.bash = ''
     # run bash, an sh-compatible shell that incorporates useful features from the Korn shell (ksh) and the C shell (csh).
     bash <args>
