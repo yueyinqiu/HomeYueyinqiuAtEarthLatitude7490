5003f01d3ad09209a9c617276d0569589e420d0c
yueyinqiu <yueyinqiu@outlook.com>
2026-08-07 11:40:23 +0800

    ld

--- src/packages/udiskie/cheats/list-devices.cs
+++ src/packages/udiskie/cheats/list-devices.cs
@@ -10,6 +10,21 @@ await new Suggester().RunAsync();
 
 class Suggester : SnaviArgumentSuggester
 {
+    private static Dictionary<string, string> GetMountMap()
+    {
+        var map = new Dictionary<string, string>();
+        foreach (var line in File.ReadAllLines("/proc/mounts"))
+        {
+            var parts = line.Split(' ', 3);
+            string dev = parts[0];
+            string mount = parts[1].Replace(@"\040", " ");
+            if (map.TryGetValue(dev, out var current) && current.Length < mount.Length)
+                continue;
+            map[dev] = mount;
+        }
+        return map;
+    }
+
     public override async IAsyncEnumerable<(string Value, string Description)> SuggestAsync(
         IReadOnlyList<string> givenArguments,
         DirectoryInfo currentDirectory,
@@ -17,14 +32,22 @@ class Suggester : SnaviArgumentSuggester
         [EnumeratorCancellation] CancellationToken cancellationToken
     )
     {
-        var output = await Cli.Wrap("lsblk")
-            .WithArguments(["-l", "-n", "-o", "NAME"])
-            .ExecuteBufferedAsync(cancellationToken);
-        foreach (var line in output.StandardOutput.Split(
-            Environment.NewLine,
-            StringSplitOptions.RemoveEmptyEntries))
+        var mounts = GetMountMap();
+        foreach (var block in new DirectoryInfo("/sys/block").EnumerateDirectories())
         {
-            yield return (line, "");
+            if (block.Name.StartsWith("loop") || block.Name.StartsWith("ram") || block.Name.StartsWith("zram"))
+                continue;
+            
+            foreach (var part in block.EnumerateDirectories())
+            {
+                if (File.Exists(Path.Combine(part.FullName, "partition")))
+                {
+                    var dev = $"/dev/{part.Name}";
+                    if (!mounts.TryGetValue(dev, out var mount))
+                        mount = "Not Mounted";
+                    yield return (dev, $"{mount}");
+                }
+            }
         }
     }
 }
