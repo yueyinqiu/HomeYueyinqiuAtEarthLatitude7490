293c05bfb46596cd94274cf65b6b82cf1fdb4366
yueyinqiu <yueyinqiu@outlook.com>
2026-08-13 17:40:02 +0800

    b

--- src/packages/proxy-to-pjlab/default.nix
+++ src/packages/proxy-to-pjlab/default.nix
@@ -20,26 +20,26 @@
     # proxy: socks5h://127.0.0.1:59553
   '';
 
-  services.podman.containers.pjlab-atrust = {
-    image = "docker.io/hagb/docker-atrust";
-    autoStart = false;
-
-    ports = [
-      "127.0.0.1:52495:5901"
-      "127.0.0.1:59553:1080"
-    ];
-
-    environment = {
-      URLWIN = "1";
-      PASSWORD = "vnc";
-    };
-
-    devices = [ "/dev/net/tun" ];
-    addCapabilities = [ "NET_ADMIN" ];
-
-    extraPodmanArgs = [
-      "--sysctl=net.ipv4.conf.default.route_localnet=1"
-      "--dns=114.114.114.114"
-    ];
-  };
+  # services.podman.containers.pjlab-atrust = {
+  #   image = "docker.io/hagb/docker-atrust";
+  #   autoStart = false;
+# 
+  #   ports = [
+  #     "127.0.0.1:52495:5901"
+  #     "127.0.0.1:59553:1080"
+  #   ];
+# 
+  #   environment = {
+  #     URLWIN = "1";
+  #     PASSWORD = "vnc";
+  #   };
+# 
+  #   devices = [ "/dev/net/tun" ];
+  #   addCapabilities = [ "NET_ADMIN" ];
+# 
+  #   extraPodmanArgs = [
+  #     "--sysctl=net.ipv4.conf.default.route_localnet=1"
+  #     "--dns=114.114.114.114"
+  #   ];
+  # };
 }
