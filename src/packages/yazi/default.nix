72f6cd4da028b23b9cff8330c4c8958fbf727157
yueyinqiu <yueyinqiu@outlook.com>
2026-08-19 16:52:05 +0800

    yazi rubbish

--- src/packages/yazi/default.nix
+++ src/packages/yazi/default.nix
@@ -1,6 +1,13 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.yazi.enable = true;
   programs.yazi.enableBashIntegration = true;
+  programs.yazi.plugins = {
+    recycle-bin = {
+      package = pkgs.yaziPlugins.recycle-bin;
+      setup = true;
+    };
+  };
+
   imports = [
     ./cheats
   ];
