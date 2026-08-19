c2c11f3b14fc5835b4df1cd3eb4566fdc2595053
yueyinqiu <yueyinqiu@outlook.com>
2026-08-19 17:11:27 +0800

    rm plugin

--- src/packages/yazi/default.nix
+++ src/packages/yazi/default.nix
@@ -1,13 +1,6 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.yazi.enable = true;
   programs.yazi.enableBashIntegration = true;
-  programs.yazi.plugins = {
-    recycle-bin = {
-      package = pkgs.yaziPlugins.recycle-bin;
-      setup = true;
-    };
-  };
-
   imports = [
     ./cheats
   ];
