37993437252b28cd8bf7282e552b4231fc0f953f
yueyinqiu <yueyinqiu@outlook.com>
2026-08-20 12:33:22 +0800

    hm

--- src/default.nix
+++ src/default.nix
@@ -6,8 +6,6 @@
   home.username = "yueyinqiu";
   home.homeDirectory = "/home/yueyinqiu";
 
-  programs.home-manager.enable = true;
-
   nixpkgs.config.allowUnfree = true;
 
   home.stateVersion = "26.05";
---
+++ src/packages/home-manager/default.nix
@@ -0,0 +1,6 @@
+{ ... }: {
+  programs.home-manager.enable = true;
+  services.home-manager.autoExpire.enable = true;
+  services.home-manager.autoExpire.frequency = "weekly";
+  services.home-manager.autoExpire.timestamp = "-7 days";
+}
