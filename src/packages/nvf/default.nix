82627fe7b2e6ea744432301f13688490ca8eddaf
yueyinqiu <yueyinqiu@outlook.com>
2026-08-24 20:34:42 +0800

    t

--- src/packages/nvf/default.nix
+++ src/packages/nvf/default.nix
@@ -11,6 +11,22 @@
       vim.lsp = {
         enable = true;
       };
+
+      # Python development (LSP/format/debug/diagnostics)
+      vim.languages = {
+        enableTreesitter = true;
+        enableFormat = true;
+        enableDAP = true;
+        enableExtraDiagnostics = true;
+
+        python = {
+          enable = true;
+          lsp.servers = [ "basedpyright" "ruff" ];
+          format.type = [ "ruff" "ruff-fix" "isort" "black" ];
+          extraDiagnostics.types = [ "mypy" ];
+          dap.debugger = [ "debugpy" ];
+        };
+      };
     };
   };
 }
