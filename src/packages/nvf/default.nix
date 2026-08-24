84e014f4ac04c7e4a5b474773f103dad964ae62e
yueyinqiu <yueyinqiu@outlook.com>
2026-08-24 21:38:17 +0800

    Revert "t"
    This reverts commit 82627fe7b2e6ea744432301f13688490ca8eddaf.

--- src/packages/nvf/default.nix
+++ src/packages/nvf/default.nix
@@ -11,22 +11,6 @@
       vim.lsp = {
         enable = true;
       };
-
-      # Python development (LSP/format/debug/diagnostics)
-      vim.languages = {
-        enableTreesitter = true;
-        enableFormat = true;
-        enableDAP = true;
-        enableExtraDiagnostics = true;
-
-        python = {
-          enable = true;
-          lsp.servers = [ "basedpyright" "ruff" ];
-          format.type = [ "ruff" "ruff-fix" "isort" "black" ];
-          extraDiagnostics.types = [ "mypy" ];
-          dap.debugger = [ "debugpy" ];
-        };
-      };
     };
   };
 }
