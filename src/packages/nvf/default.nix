aa491ec112d0b6fe0349fe385d1ebd882aa508e4
yueyinqiu <yueyinqiu@outlook.com>
2026-08-27 22:53:42 +0800

    cs

--- src/packages/nvf/default.nix
+++ src/packages/nvf/default.nix
@@ -61,7 +61,13 @@
       xml.enable = true;
       env.enable = true;
 
-      csharp.enable = true;
+      csharp = {
+        enable = true;
+        lsp.servers = ["roslyn-ls"];
+        extensions.roslyn-nvim.enable = true;
+        # https://github.com/NotAShelf/nvf/issues/1673
+        extensions.roslyn-nvim.setupOpts.extensions.razor.enabled = false;
+      };
     };
 
     visuals = {
