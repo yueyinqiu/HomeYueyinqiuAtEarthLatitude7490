234da622a6a3b2f05af27971258c0f2ec0751ac7
yueyinqiu <yueyinqiu@outlook.com>
2026-09-04 13:24:06 +0800

    f

--- src/packages/vscode/default.nix
+++ src/packages/vscode/default.nix
@@ -24,19 +24,15 @@
       ms-dotnettools.csdevkit
 
       jnoortheen.nix-ide
+
+      yy0931.vscode-sqlite3-editor
     ]
     ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
         name = "remote-explorer";
         publisher = "ms-vscode";
         version = "0.5.0";
-        sha256 = "1hp6gjh4xp2m1xxxxjsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }
-      {
-        name = "sqlite-viewer";
-        publisher = "qwtel";
-        version = "26.8.0";
-        sha256 = "1hp6gjh4xp2m1xxxxjsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        sha256 = "sha256-BNsnetpddxv3Y9MjZERU5jOq1I2g6BNFF1rD7Agpmr8=";
       }
       {
         name = "visual-studio-2019-themes-extracted-from-csharp-extension";
