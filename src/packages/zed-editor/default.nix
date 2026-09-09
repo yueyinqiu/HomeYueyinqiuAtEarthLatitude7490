34977ef026e058cb66604abd48bbc39115253b76
yueyinqiu <yueyinqiu@outlook.com>
2026-09-09 19:19:53 +0800

    Update default.nix

--- src/packages/zed-editor/default.nix
+++ src/packages/zed-editor/default.nix
@@ -1,14 +1,8 @@
 { pkgs, ... }: {
   programs.zed-editor.enable = true;
   programs.zed-editor.userSettings = {
-    lsp.roslyn.binary = {
-      "path" = "${pkgs.roslyn-ls}/bin/Microsoft.CodeAnalysis.LanguageServer";
-      "arguments" = [ "--stdio" ];
-    };
+    lsp.roslyn.binary.path = "${pkgs.roslyn-ls}/bin/Microsoft.CodeAnalysis.LanguageServer";
   };
-  programs.zed-editor.extraPackages = [
-    pkgs.bubblewrap
-  ];
   
   home.packages = [
     (pkgs.writeShellApplication {
