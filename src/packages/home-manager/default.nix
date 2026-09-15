efbf811f465cf318199466334098d87a83f55630
yueyinqiu <yueyinqiu@outlook.com>
2026-09-15 20:37:28 +0800

    better expire

--- src/packages/home-manager/default.nix
+++ src/packages/home-manager/default.nix
@@ -1,8 +1,5 @@
 { ... }: {
   programs.home-manager.enable = true;
-  services.home-manager.autoExpire.enable = true;
-  services.home-manager.autoExpire.frequency = "weekly";
-  services.home-manager.autoExpire.timestamp = "-7 days";
   
   imports = [
     ./cheats
