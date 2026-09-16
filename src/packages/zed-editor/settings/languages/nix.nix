c6e47b4444f2aa0983c46880b7dd461ff4a10a53
yueyinqiu <yueyinqiu@outlook.com>
2026-09-16 15:15:20 +0800

    Update nix.nix

--- src/packages/zed-editor/settings/languages/nix.nix
+++ src/packages/zed-editor/settings/languages/nix.nix
@@ -1,5 +1,11 @@
 { ... }:
 {
+  programs.zed-editor.userSettings.lsp.nixd = {
+    binary = {
+      path = "${pkgs.nixd}/bin/nixd";
+    };
+  };
+  
   programs.zed-editor.userSettings.languages.Nix = {
     language_servers = [ "nixd" ];
   };
