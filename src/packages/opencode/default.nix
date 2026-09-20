fbb7e60a8635aa68b51385ce6c59c691393f4d69
yueyinqiu <yueyinqiu@outlook.com>
2026-09-20 15:20:01 +0800

    no thinking no other models

--- src/packages/opencode/default.nix
+++ src/packages/opencode/default.nix
@@ -3,7 +3,6 @@
     opencode
   ];
   xdg.configFile."opencode/opencode.jsonc".source = ./opencode.jsonc;
-  xdg.configFile."opencode/plugin/remove_max_tokens.ts".source = ./remove_max_tokens.ts;
 
   imports = [
     ./cheats.nix
