25a285551dc5a5b72286c2d288c2ce76e440f0b3
yueyinqiu <yueyinqiu@outlook.com>
2026-09-20 14:45:26 +0800

    ok firefox proxy

--- src/packages/firefox/default.nix
+++ src/packages/firefox/default.nix
@@ -1,16 +1,6 @@
 {pkgs, ...}: {
   programs.firefox.enable = true;
-
-  my.proxies.for-firefox = {
-    files = [
-      ./proxy/config.sh.example
-      ./proxy/remove-proxies.yaml
-      ./proxy/update-proxy-groups.js
-      ./proxy/prepend-rules.yaml
-    ];
-    port = 10538;
-  };
-
+  
   programs.home-manager-mihomo-manager.instances.for-firefox = {
     port = 37260;
     configuration = ./proxy-config;
