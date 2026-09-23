0ced18b6058476476a40404989c10c32570352d4
yueyinqiu <yueyinqiu@outlook.com>
2026-09-23 13:30:36 +0800

    x

--- src/packages/fonts/default.nix
+++ src/packages/fonts/default.nix
@@ -30,7 +30,7 @@
     };
 
     configFile.yahei-no-ui = {
-      enable = true;
+      enable = false;
       text = ''
         <?xml version="1.0"?>
         <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
