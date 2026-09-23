76b51e7ad580355575070b6760be8ab35ed28d0d
yueyinqiu <yueyinqiu@outlook.com>
2026-09-23 17:04:52 +0800

    zz

--- src/packages/vscode/settings/default.nix
+++ src/packages/vscode/settings/default.nix
@@ -62,19 +62,6 @@
 
       "window.restoreWindows" = "none";
 
-      "[python]" = {
-        "editor.autoClosingQuotes" = "languageDefined";
-        "editor.autoIndent" = "advanced";
-      };
-      "python.analysis.autoFormatStrings" = true;
-      "python.analysis.autoImportCompletions" = true;
-      "python.analysis.diagnosticMode" = "workspace";
-      "python.analysis.inlayHints.functionReturnTypes" = true;
-      "python.analysis.inlayHints.pytestParameters" = true;
-      "python.analysis.typeCheckingMode" = "standard";
-      "python.terminal.activateEnvironment" = false;
-      "flake8.enabled" = false;
-
       # --- misc ---
       "chat.disableAIFeatures" = true;
       "direnv.watchForChanges" = false;
---
+++ src/packages/vscode/settings/languages/python.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  programs.vscode.profiles.default.userSettings = {
+      "[python]" = {
+        "editor.autoClosingQuotes" = "languageDefined";
+        "editor.autoIndent" = "advanced";
+      };
+      "python.analysis.autoFormatStrings" = true;
+      "python.analysis.autoImportCompletions" = true;
+      "python.analysis.diagnosticMode" = "workspace";
+      "python.analysis.inlayHints.functionReturnTypes" = true;
+      "python.analysis.inlayHints.pytestParameters" = true;
+      "python.analysis.typeCheckingMode" = "standard";
+      "python.terminal.activateEnvironment" = false;
+      "flake8.enabled" = false;
+  };
+}
