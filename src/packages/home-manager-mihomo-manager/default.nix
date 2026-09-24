880f7266922b4175cce107fae90dfe7c1816d99e
yueyinqiu <yueyinqiu@outlook.com>
2026-09-24 14:03:27 +0800

    h

--- src/packages/home-manager-mihomo-manager/default.nix
+++ src/packages/home-manager-mihomo-manager/default.nix
@@ -1,5 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.home-manager-mihomo-manager.enable = true;
+
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "m";
+      text = ''
+        exec home-manager-mihomo-manager "$@"
+      '';
+    })
+  ];
 
   imports = [
     ./cheats.nix
